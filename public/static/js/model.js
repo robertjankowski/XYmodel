9668ed5c00a04f1312c3d903f692b4a0aefc074b
robertjankowski <robjanw11@wp.pl>
2018-12-12 20:58:16 +0100

    footer and spin template

--- public/static/js/model.js
+++ public/static/js/model.js
@@ -2,7 +2,14 @@ var view = document.getElementById("model")
 var rendered = PIXI.autoDetectRenderer(400, 400, {view});
 var stage = new PIXI.Container();
 
-render();
+const arrowImagePath = "../images/arrow.png"
+PIXI.loader
+    .add(arrowImagePath)
+    .load(render)
+
+// let img = new PIXI.Sprite(PIXI.loader.resources[arrowPath].texture)
+// var spin = new Spin(0,0,0);
+
 
 function render() {
     rendered.render(stage);
--- public/static/js/utils.js
+++ public/static/js/utils.js
@@ -1,13 +1,4 @@
 // Temperature slider setup
-$(function () {
-    var handle = $("#custom-handle");
-    $("#slider").slider({
-        create: function () {
-            handle.text($(this).slider("value"));
-        },
-        slide: function (event, ui) {
-            handle.text(ui.value);
-        }
-    });
-});
+
+
 // Grid size slider setup
