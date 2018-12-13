98d91501b41d55d9cabc738fd88435fd014eac1e
robertjankowski <robjanw11@wp.pl>
2018-12-13 14:52:10 +0100

    attempt to sliders

--- public/static/js/utils.js
+++ public/static/js/utils.js
@@ -2,3 +2,17 @@
 
 
 // Grid size slider setup
+
+// Test $ is not a function ...
+// $("#example-id").ionRangeSlider({
+    // min: 0,
+    // max: 2,
+    // from: 1
+// });
+
+var $jq = jQuery.noConflict();
+$jq("#example-id").ionRangeSlider({
+    min: 0,
+    max: 100,
+    from: 20
+});
