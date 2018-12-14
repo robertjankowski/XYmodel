78f2f99fc7894333df60755010a9ba4c724e7772
robertjankowski <robjanw11@wp.pl>
2018-12-14 10:36:51 +0100

    fix sliders

--- public/static/js/model.js
+++ public/static/js/model.js
@@ -2,13 +2,6 @@ var view = document.getElementById("model")
 var rendered = PIXI.autoDetectRenderer(400, 400, {view});
 var stage = new PIXI.Container();
 
-const arrowImagePath = "../images/arrow.png"
-PIXI.loader
-    .add(arrowImagePath)
-    .load(render)
-
-// let img = new PIXI.Sprite(PIXI.loader.resources[arrowPath].texture)
-// var spin = new Spin(0,0,0);
 
 
 function render() {
--- public/static/js/utils.js
+++ public/static/js/utils.js
@@ -1,18 +1,23 @@
 // Temperature slider setup
-
+$(".temperature").ionRangeSlider({
+    min: 0,
+    max: 2,
+    step: 0.05,
+    from: 1.5,
+    skin: 'round',
+    onChange: function (data) {
+        console.log(data.from);
+    }
+});
 
 // Grid size slider setup
-
-// Test $ is not a function ...
-// $("#example-id").ionRangeSlider({
-    // min: 0,
-    // max: 2,
-    // from: 1
-// });
-
-var $jq = jQuery.noConflict();
-$jq("#example-id").ionRangeSlider({
-    min: 0,
-    max: 100,
-    from: 20
+$(".grid").ionRangeSlider({
+    min: 32,
+    max: 256,
+    step: 8,
+    from: 128,
+    skin: 'round',
+    onChange: function(data) {
+        console.log(data.from);
+    }
 });
