a70437d73f7400ae60fef39670e1f47e60f9c16b
robertjankowski <robjanw11@wp.pl>
2018-12-21 10:14:00 +0100

    add update method

--- public/static/js/model.js
+++ public/static/js/model.js
@@ -1,182 +1,212 @@
-// colors
-var BLACK_COLOR = 0x000000;
-var WHITE_COLOR = 0xffffff;
-var EDGE_COLOR = 0x2679ff; // blue
-var GRID_COLOR = 0xa1aab7; // gray
-
 // initialize variable
-const GRID_SIZE = 600;
+var GRID_SIZE = 600; // window size
 var SIZE_CELL;
 var CELL_HEIGHT_WIDTH;
 var LINE_GRID_WIDTH = 1;
-var LINE_EDGE = 2;
+var LINE_EDGE = 4;
+var WHITE = 'rgb(255, 255, 255)';
+var BLACK = 'rgb(0, 0, 0)';
 
-//
+// cell variables
 var total_cells;
-var cell_state;
-var go_button_counter = 0;
-var timeout;
+var cellStates;
+var buttonCounter = 0;
+var TimeoutID;
 
-// pixi.js variables
-var view;
-var rendered;
-var stage;
-var graphics;
-var ticker;
+// graphics
+var context;
 
-function init_grid() {
+window.onload = function xy_model() {
+
+    makeCanvas();
+    initCellState(cellStates);
+    // attach buttons listeners
+    document.getElementById("setup").addEventListener("click", setup);
+    document.getElementById("go").addEventListener("click", go);
+
+}
+
+function setup() {
+    makeCanvas();
+    cellStates = initCellState();
+    initRandomCells(cellStates);
+    drawCells(cellStates);
+    console.log("SETUP");
+}
+
+function go() {
+    if (buttonCounter % 2 === 0) {
+        // for loop simulation
+        document.getElementById('setup').disabled = true;
+        updateGrid();
+    } else {
+        // stop simulation
+        document.getElementById('setup').disabled = false;
+    }
+    buttonCounter++;
+}
+
+
+function initGrid() {
     SIZE_CELL = parseInt(document.getElementById("size_cell").value);
     CELL_HEIGHT_WIDTH = GRID_SIZE / SIZE_CELL;
     total_cells = CELL_HEIGHT_WIDTH * CELL_HEIGHT_WIDTH;
 }
 
-function make_grid() {
-    graphics.lineStyle(LINE_EDGE, BLACK_COLOR);
-    graphics.drawRect(0, 0, GRID_SIZE, GRID_SIZE);
+function makeCanvas() {
+    canvas = document.getElementById("model");
+    canvas.width = GRID_SIZE;
+    context = canvas.getContext('2d');
+
+    initGrid();
+    context.beginPath();
+    context.fillStyle = WHITE;
+    context.fillRect(0, 0, GRID_SIZE, GRID_SIZE);
+    makeGrid();
+}
+
+function makeGrid() {
+    context.lineWidth = LINE_GRID_WIDTH;
+    context.strokeRect(0, 0, GRID_SIZE, GRID_SIZE);
     for (var i = 0; i < CELL_HEIGHT_WIDTH - 1; i++) {
-        graphics.lineStyle(LINE_GRID_WIDTH, GRID_COLOR);
-        // from https://stackoverflow.com/a/7531540/9511702
-        graphics.moveTo(SIZE_CELL + i * SIZE_CELL + 0.5, 0);
-        graphics.lineTo(SIZE_CELL + i * SIZE_CELL + 0.5, GRID_SIZE);
-        graphics.closePath();
+        context.beginPath();
+        context.lineWidth = LINE_GRID_WIDTH;
+        context.strokeStyle = BLACK;
+        context.moveTo(SIZE_CELL + i * SIZE_CELL, 0);
+        context.lineTo(SIZE_CELL + i * SIZE_CELL, GRID_SIZE);
+        context.stroke();
     }
     for (var j = 0; j < CELL_HEIGHT_WIDTH - 1; j++) {
-        graphics.lineStyle(LINE_GRID_WIDTH, GRID_COLOR);
-        graphics.moveTo(0, SIZE_CELL + j * SIZE_CELL + 0.5);
-        graphics.lineTo(GRID_SIZE, SIZE_CELL + j * SIZE_CELL + 0.5);
-        graphics.closePath();
+        context.beginPath();
+        context.lineWidth = LINE_GRID_WIDTH;
+        context.strokeStyle = BLACK;
+        context.moveTo(0, SIZE_CELL + j * SIZE_CELL);
+        context.lineTo(GRID_SIZE, SIZE_CELL + j * SIZE_CELL);
+        context.stroke();
     }
-}
-
-function create_canvas() {
-    view = document.getElementById("model");
-    rendered = PIXI.autoDetectRenderer(GRID_SIZE, GRID_SIZE, {
-        view: view
-    });
-    stage = new PIXI.Container();
-    ticker = new PIXI.ticker.Ticker()
-    // draw grid
-    init_grid();
-    graphics = new PIXI.Graphics();
-    graphics.beginFill(WHITE_COLOR);
-    graphics.drawRect(0, 0, GRID_SIZE, GRID_SIZE);
-    graphics.endFill();
-    make_grid();
-    stage.addChild(graphics);
-    rendered.render(stage);
-}
-
-function setup() {
-    create_canvas();
-    init_cells_state();
-    random_init_cells();
-    draw_cells();
-    rendered.render(stage);
-    ticker.add((deltaTime) => {
-        update(); // TODO: not rendering updated cell_state! 
-        rendered.render(stage);
-    });
-    console.log("SETUP");
-}
-
-function go() {
-    if (go_button_counter % 2 === 0) {
-        // forever loop simulation
-        document.getElementById("setup").disabled = true;
-        ticker.start();
-
-    } else {
-        // stop simulation
-        document.getElementById("setup").disabled = false;
-        ticker.stop();
-    }
-    go_button_counter++;
+    console.log("MAKE_GRID");
 }
 
 
-function update() {
-    var T = parseFloat(document.getElementById("temp_id").value);
-    var J = parseFloat(document.getElementById("interaction_id").value);
-
-    const fps = 50;
-
+function initCellState() {
+    cellStates = new Array();
     for (var i = 0; i < CELL_HEIGHT_WIDTH; i++) {
+        cellStates[i] = new Array();
         for (var j = 0; j < CELL_HEIGHT_WIDTH; j++) {
-
-            // TODO: implement Metropolis algorithm
-            var delta_E = 0;
-
-            var exp_boltzmann = 0;
-            if (T != 0) {
-                exp_boltzmann = Math.exp(-delta_E / T);
-            }
-
+            cellStates[i][j] = 0;
         }
     }
-    // for tests
-    random_init_cells();
+    return cellStates;
 }
 
-function draw_cells() {
-    // for arrows
-    var A1 = new Array(2);
-    var B1 = new Array(2);
-    var A2 = new Array(2);
-    var B2 = new Array(2);
-    for (var i = 0; i < CELL_HEIGHT_WIDTH; i++) {
-        for (var j = 0; j < CELL_HEIGHT_WIDTH; j++) {
-            var xx = i * SIZE_CELL;
-            var yy = j * SIZE_CELL;
-
-            // map angle to color
-            var red = Math.floor(
-                255 * (Math.cos(cell_state[i][j]) + 1) / 2
-            );
-            var green = Math.floor(
-                255 * (Math.sin(cell_state[i][j]) + 1) / 2
-            );
-            var blue = Math.floor(
-                255 * (-Math.cos(cell_state[i][j]) + 1) / 2
-            );
-            var alpha = 0.75;
-            var color = "rgba(" + red + "," + green + "," + blue + "," + alpha + ")";
-            var hex = rgb2hex(color);
-            var hexInt = '0x' + hex.replace(/^#/, '');
-                
-            graphics.beginFill(hexInt);
-            graphics.drawRect(xx, yy, SIZE_CELL, SIZE_CELL);
-            graphics.endFill();
-            
-        }
-    }
-}
-
-function init_cells_state() {
-    cell_state = new Array();
-    for (var i = 0; i < CELL_HEIGHT_WIDTH; i++) {
-        cell_state[i] = new Array();
-        for (var j = 0; j < CELL_HEIGHT_WIDTH; j++) {
-            cell_state[i][j] = 0;
-        }
-    }
-}
-
-function random_init_cells() {
+function initRandomCells(cell_state) {
     for (var i = 0; i < CELL_HEIGHT_WIDTH; i++) {
         for (var j = 0; j < CELL_HEIGHT_WIDTH; j++) {
             // range (-pi, pi)
-            cell_state[i][j] = Math.PI * (2 * Math.random() - 1); 
+            cell_state[i][j] = Math.PI * (2 * Math.random() - 1);
         }
     }
 }
 
-window.onload = function xy_model() {
+function drawCells(cell_state) {
+    for (var i = 0; i < CELL_HEIGHT_WIDTH; i++) {
+        for (var j = 0; j < CELL_HEIGHT_WIDTH; j++) {
+            var x = i * SIZE_CELL;
+            var y = j * SIZE_CELL;
 
-    create_canvas();
-    init_cells_state();
+            context.beginPath();
+            // map cell value to color
+            var red = Math.floor(255 * (Math.cos(cellStates[i][j]) + 1) / 2);
+            var green = Math.floor(255 * (Math.sin(cellStates[i][j]) + 1) / 2);
+            var blue = Math.floor(255 * (-Math.cos(cellStates[i][j]) + 1) / 2);
+            var rgba = "rgba(" + red + "," + green + "," + blue + ",0.75)";
+            context.fillStyle = rgba;
+            context.fillRect(x, y, SIZE_CELL, SIZE_CELL);
+        }
+    }
+}
 
-    // attach buttons listeners
-    document.getElementById("setup").addEventListener("click", setup);
-    document.getElementById("go").addEventListener("click", go);
+var miiin;
+var piiin;
+var jjin;
+var mjjin;
+var pjjin;
 
+function updateGrid() {
+    var T = parseFloat(document.getElementById("temp_id").value);
+    var J = parseFloat(document.getElementById("interaction_id").value);
+
+    // TODO: add arrows and think about algorithms
+    
+    for (var i = 0; i < CELL_HEIGHT_WIDTH; i++) {
+        for (var j = 0; j < CELL_HEIGHT_WIDTH; j++) {
+
+            var ii = Math.floor(Math.random() * CELL_HEIGHT_WIDTH);
+            var jj = Math.floor(Math.random() * CELL_HEIGHT_WIDTH);
+
+            var iiin = i;
+            miiin = i - 1;
+            piiin = i + 1;
+            jjin = j;
+            mjjin = j - 1;
+            pjjin = j + 1;
+
+            boundaryConditions();
+
+            var mii = miiin;
+            var pii = piiin;
+            var mjj = mjjin;
+            var pjj = pjjin;
+
+            var trialCellState = cellStates[ii][jj] + 0.25 * Math.PI * (2 * Math.random() - 1);
+            if (trialCellState >= Math.PI) {
+                trialCellState = trialCellState - 2 * Math.PI;
+            } else if (trialCellState < -Math.PI) {
+                trialCellState = trialCellState + 2 * Math.PI;
+            }
+
+            var deltaE = -J * (Math.cos(trialCellState - cellStates[mii][jj]) +
+                    Math.cos(trialCellState - cellStates[pii][jj]) +
+                    Math.cos(trialCellState - cellStates[ii][mjj]) +
+                    Math.cos(trialCellState - cellStates[ii][pjj])) +
+                J * (Math.cos(cellStates[ii][jj] - cellStates[mii][jj]) +
+                    Math.cos(cellStates[ii][jj] - cellStates[pii][jj]) +
+                    Math.cos(cellStates[ii][jj] - cellStates[ii][mjj]) +
+                    Math.cos(cellStates[ii][jj] - cellStates[ii][pjj]))
+
+            var expBoltzmann = 0;
+            if (T != 0) {
+                expBoltzmann = Math.exp(-deltaE / T);
+            }
+            if (deltaE > 0) {
+                if (Math.random() < expBoltzmann) {
+                    cellStates[ii][jj] = trialCellState;
+                }
+            } else {
+                cellStates[ii][jj] = trialCellState;
+            }
+        }
+    }
+    drawCells(cellStates);
+
+    var fps = 100;
+    TimeoutID = setTimeout(updateGrid, 1000 / fps, cellStates); //1000ms
+}
+
+function boundaryConditions() {
+    var mii = miiin;
+    var pii = piiin;
+    var mjj = mjjin;
+    var pjj = pjjin;
+
+    //periodic boundary condition
+    if (mii < 0) mii = mii + CELL_HEIGHT_WIDTH;
+    if (pii > CELL_HEIGHT_WIDTH - 1) pii = pii - CELL_HEIGHT_WIDTH;
+    if (mjj < 0) mjj = mjj + CELL_HEIGHT_WIDTH;
+    if (pjj > CELL_HEIGHT_WIDTH - 1) pjj = pjj - CELL_HEIGHT_WIDTH;
+
+    miiin = mii;
+    piiin = pii;
+    mjjin = mjj;
+    pjjin = pjj;
 }
